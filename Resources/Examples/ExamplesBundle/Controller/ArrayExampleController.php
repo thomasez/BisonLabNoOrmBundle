f214c2071f959fce4ded955670adfabd24980c93
Dabaru Application User <dabaru@dabaru-dev.nteb.no>
2018-07-03 21:42:01 +0200

    New routing component

--- Resources/Examples/ExamplesBundle/Controller/ArrayExampleController.php
+++ Resources/Examples/ExamplesBundle/Controller/ArrayExampleController.php
@@ -9,7 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class ArrayExampleController extends BaseController
